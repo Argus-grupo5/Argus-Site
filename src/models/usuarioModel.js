8e1617600f0b0035785ae494d03eb377f07cec95
Ally4wada <ally.awada@sptech.school>
2025-08-25 12:14:49 -0300

    Fix: Correção na inserção de dados no banco

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -23,13 +23,13 @@ function cadastrarEndereco(cep, rua, bairro, cidade, estado, numero, complemento
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrarEmpresa(nome, email, senha, cnpj, razao, fkEndereco) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cnpj, razao, fkEndereco);
+function cadastrarEmpresa(nome, telefone, email, senha, cnpj, razao, fkEndereco) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, telefone, email, senha, cnpj, razao, fkEndereco);
 
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO empresa (nome_fantasia, email, senha, cnpj, razao_social, fkEndereco) VALUES ('${nome}', '${email}', SHA2('${senha}', 512), '${razao}', ${cnpj}, ${fkEndereco});
+        INSERT INTO empresa (nome_fantasia, telefone, email, senha, cnpj, razao_social, fkEndereco) VALUES ('${nome}', '${telefone}', '${email}', SHA2('${senha}', 512), '${razao}', ${cnpj}, ${fkEndereco});
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
