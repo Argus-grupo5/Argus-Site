acdb66cad4fff4732192efefdade2528d8250e98
Ally4wada <ally.awada@sptech.school>
2025-08-25 13:56:01 -0300

    fix: ajustes na página de cadastro

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT id, nome, email FROM usuarios where email = '${email}' AND senha = '${senha}';
+        SELECT * from vw_user WHERE email = '${email}' AND senha = SHA2('${senha}', 512);
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
