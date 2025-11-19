b1f0add4932a07d38041c62f67fd994df213619f
EduardoNdL <eduardo.lima@sptech.school>
2025-11-19 14:50:30 -0300

    feat(s3) add infos do s3

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var empresasRouter = require("./src/routes/empresas");
 var servidoresRouter = require("./src/routes/servidores")
+var s3Router = require("./src/routes/s3Route");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,6 +31,8 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/empresas", empresasRouter);
 app.use("/servidores", servidoresRouter);
+app.use('/s3Route', s3Router);
+
 
 app.listen(PORTA_APP, function () {
     console.log(`
