9a8436cd65afdb633b54143f3544f56e6d931585
Fernanda <fernanda.araujo@sptech.school>
2025-11-21 21:40:12 -0300

    fix pegando os dados do jira para mostrar na dashboard

--- src/controllers/jiraController.js
+++ src/controllers/jiraController.js
@@ -1,50 +1,37 @@
-const axios = require("axios");
+const email_jira = "luiz.hsouza@sptech.school";
+const token_jira = "ATATT3xFfGF08peCbzHDW7NphdJZSbDE73t0jK9txkqai4zucwrE2PQFfmpkoD-GbPoZKzxrFYk51KYdU4IqErcyxUOpBVTP7-JNY56aOmW2ugAKchMZ_47RFtaj5M6JAGPi64A5aQjbJzsQAe5DauvRlZ-zOHVdVhPiEftcuwCEAQR3EHCafgk=D75C1533";
+const dominio_jira = "sptech-team-cppjq4lc.atlassian.net";
+const auth = Buffer.from(`${email_jira}:${token_jira}`).toString("base64");
 
 async function getResumoJira(req, res) {
-    try {
-        const email = process.env.JIRA_EMAIL;
-        const token = process.env.JIRA_TOKEN;
+  try {
+    const url = `https://${dominio_jira}/rest/api/3/search/jql`;
+    const resposta = await fetch( url,
+      {
+        method: "POST",
+        headers: {
+          "Authorization": `Basic ${auth}`,
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          jql: "project = AA",
+          maxResults: 125,
+          fields: ["summary", "assignee", "priority", "status",  "created", "resolutiondate"]
+        })
+      });
 
-        const auth = Buffer.from(`${email}:${token}`).toString('base64');
-
-        const resposta = await axios.post(
-            "https://sptech-team-cppjq4lc.atlassian.net/rest/api/3/search/jql",
-            {
-                jql: "project = AA",
-                maxResults: 50
-            },
-            {
-                headers: {
-                    "Authorization": `Basic ${auth}`,
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                }
-            }
-        );
-
-        const issues = resposta.data.issues;
-
-        const abertas = issues.filter(i => i.fields.status.name !== "Done").length;
-        const fechadas = issues.filter(i => i.fields.status.name === "Done").length;
-
-        const prioridade = {
-            high: issues.filter(i => i.fields.priority?.name === "High").length,
-            medium: issues.filter(i => i.fields.priority?.name === "Medium").length,
-            low: issues.filter(i => i.fields.priority?.name === "Low").length
-        };
-
-        const total = issues.length;
-
-        res.json({
-            total,
-            abertas,
-            fechadas,
-            prioridades: prioridade
-        });
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ erro: "Falha ao consultar Jira" });
+      if (!resposta.ok) {
+      throw new Error(`Erro na API do Jira: ${resposta.status} ${resposta.statusText}`);
     }
+
+    const dados = await resposta.json();
+    res.status(200).json(dados);
+  } catch (erro) {
+    console.error("Erro ao buscar chamados do Jira:", erro);
+    res.status(500).json({ error: "Erro ao buscar chamados do Jira" });
+  }
 }
-module.exports = { getResumoJira };
+module.exports = {
+  getResumoJira
+};
