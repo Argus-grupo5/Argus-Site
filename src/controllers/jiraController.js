021a38469d3183fd0514e97cba7e68684fac654f
Fernanda <fernanda.araujo@sptech.school>
2025-11-25 14:31:12 -0300

    del token

--- src/controllers/jiraController.js
+++ src/controllers/jiraController.js
@@ -1,5 +1,5 @@
 const email_jira = "luiz.hsouza@sptech.school";
-const token_jira = "ATATT3xFfGF08peCbzHDW7NphdJZSbDE73t0jK9txkqai4zucwrE2PQFfmpkoD-GbPoZKzxrFYk51KYdU4IqErcyxUOpBVTP7-JNY56aOmW2ugAKchMZ_47RFtaj5M6JAGPi64A5aQjbJzsQAe5DauvRlZ-zOHVdVhPiEftcuwCEAQR3EHCafgk=D75C1533";
+const token_jira = "";
 const dominio_jira = "sptech-team-cppjq4lc.atlassian.net";
 const auth = Buffer.from(`${email_jira}:${token_jira}`).toString("base64");
 
